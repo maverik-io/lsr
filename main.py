2ce7820c9827555c61238384796bc2f05e98bf60
User <user@Users-iMac.local>
2024-03-11 12:40:25 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+#! /usr/local/bin/python3
+import os
+
+dirs = os.listdir(".")
+
+icons = {
+    ('md',)                         : "  ",
+    ('ai',)                         : "  ",
+    ('png', 'jpeg', 'jpg', 'tiff')  : "  ",
+    ('py', 'pyc')                   : "  ",
+    ('psd',)                        : "  ",
+    ('svg',)                        : "  ",
+    }
+
+print('-='*int(os.get_terminal_size()[0]/2))
+
+
+def recursive(dirs, path='.', spaces=0):
+    for item in dirs:
+        if item[0] == '.':
+            continue
+        if os.path.isdir(path+'/'+item):
+            print(spaces*" "+"   "+item)
+            recursive(os.listdir(path+'/'+item), path+'/'+item, spaces+1)
+
+        else:
+            extension = item.split('.')[-1]
+            icon = '  '
+            for ext in icons:
+                if extension in ext:
+                    icon = icons[ext]
+            print(spaces*' '+f"  {icon}"+item)
+
+
+recursive(dirs)
+print('-='*int(os.get_terminal_size()[0]/2))
