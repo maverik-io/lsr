6746f64e52daa5733d6861f7a7d52f53e921bdc9
User <user@Users-iMac.local>
2024-03-12 12:57:47 +0530

    Add proper lines

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ icons = {
     ('py', 'pyc')                   : "  ",
     ('psd',)                        : "  ",
     ('svg',)                        : "  ",
+    ('ttf', 'otf')                  : "  ",
     }
 
 print('-='*int(os.get_terminal_size()[0]/2))
@@ -20,7 +21,7 @@ def recursive(dirs, path='.', spaces=0):
         if item[0] == '.':
             continue
         if os.path.isdir(path+'/'+item):
-            print(spaces*" "+"   "+item)
+            print(spaces*" ▏"+"   "+item)
             recursive(os.listdir(path+'/'+item), path+'/'+item, spaces+1)
 
         else:
@@ -29,8 +30,7 @@ def recursive(dirs, path='.', spaces=0):
             for ext in icons:
                 if extension in ext:
                     icon = icons[ext]
-            print(spaces*' '+f"  {icon}"+item)
-
+            print(spaces*' ▏'+f"  {icon}"+item)
 
 recursive(dirs)
 print('-='*int(os.get_terminal_size()[0]/2))
